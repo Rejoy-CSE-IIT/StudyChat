412a4a3a96bd9d9aa979260d7814e37354e23c8e
Rejoy-CSE-IIT <rejoy.projects@gmail.com>
2017-04-03 20:56:02 +0530

    added sign in by gmail_added sign out menu

--- app/src/main/java/com/geometry/chatprogramfinal/t_b_google_login/google_login_acitivity.java
+++ app/src/main/java/com/geometry/chatprogramfinal/t_b_google_login/google_login_acitivity.java
@@ -3,6 +3,9 @@ package com.geometry.chatprogramfinal.t_b_google_login;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -61,5 +64,31 @@ public class google_login_acitivity extends AppCompatActivity
     {
         super.onResume();
         mFirebaseAuth.addAuthStateListener( mAuthStateListener);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/ Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch(id)
+        {
+            case R.id.action_sign_out:
+                // Sign Out menu item click
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected( item);
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
